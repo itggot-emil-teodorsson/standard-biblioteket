05a9d5b4eaf3fe03532ca2d3c1f70d353a21ec0d
itggot-emil-teodorsson <35598572+itggot-emil-teodorsson@users.noreply.github.com>
2018-05-02 13:32:44 +0200

    Lade till Min of Four

---
+++ lib/min_of_four.rb
@@ -0,0 +1,34 @@
+# Public: Takes four numbers and checks which one is smallest.
+#
+# num1 - The first number.
+# num2 - The second number.
+# num3 - The third number.
+# num4 - The fourth number.
+#
+# Examples
+#
+#   min_of_four(1, 2, 3, 4)
+#   # => 1
+#
+# Returns the smallest of the four numbers.
+ 
+def min_of_four(num1, num2, num3, num4)
+    if num1 < num2
+        if num1 < num3
+            if num1 < num4
+                return num1
+            end
+        end
+    elsif num2 < num3
+        if num2 < num4
+            return num2
+        end
+    elsif num3 < num4
+        return num3
+    end
+    return num4
+end
+
+p min_of_four(1, 2, 3, 4) #=> 1
+p min_of_four(100, 2, 256, 1) #=> 1
+p min_of_four(1337, -1337, 0, -1338) #=> -1338
